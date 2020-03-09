6328ac83fadf3c2ad7bbe560f4ab3d4d0095e77c
davidak <git@davidak.de>
2020-03-09 11:17:24 +0100

    Add bootstrap3-jinja theme

--- conf.py
+++ conf.py
@@ -159,7 +159,7 @@ NAVIGATION_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "bootstrap3"
+THEME = "bootstrap3-jinja"
 
 # Primary color of your theme. This will be used to customize your theme and
 # auto-generate related colors in POSTS_SECTION_COLORS. Must be a HEX value.
